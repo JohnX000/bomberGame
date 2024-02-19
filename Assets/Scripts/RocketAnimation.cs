432f2ae2b68b368634042bb9f5a95fbd8311795f
JohnX000 <158768381+JohnX000@users.noreply.github.com>
2024-02-19 17:52:08 -0600

    Add files via upload

---
+++ Assets/Scripts/RocketAnimation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Animator")]
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("HAxis", horizontal);
+    }
+}
